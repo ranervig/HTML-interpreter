2d2c55fdb22ba12dc5072640f73c745ac980fba9
nmchung757 <78885485+nmchung757@users.noreply.github.com>
2021-04-09 16:31:12 -0500

    List format function
    Added the function to turn a list like:
    *OL item1, item2, item3
    Would become:
    <ol>
    <li>item1</li>
    <li>item2</li>
    <li>item3</li>
    </ol>
    I've done basic tests and it returned working HTML lists.

--- Source.cpp
+++ Source.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 deque<string> readFile();
+string stringToList(string unformattedList);
 
 int main() {
 
@@ -46,3 +47,39 @@ deque<string> readFile() {
 }
 
 
+
+string stringToList(string unformattedList) { // Noah Chung
+    // takes a string and returns a string in HTML list format that can be added to the queue
+    // tested it with items of different size and with lists with 3 and 4 items
+    const int AFTER_TAG = 4;
+    string list;
+    size_t nextComma = 0;
+    bool firstItem = true;
+
+    while (nextComma != string::npos) {
+        string dimension;
+        size_t comma = unformattedList.find(", ", nextComma);
+        nextComma = unformattedList.find(", ", nextComma + 2);
+
+        if (firstItem == true) {
+            list = list + "<li>" + unformattedList.substr(AFTER_TAG, nextComma - 4) + "</li>" + "\n";
+        }
+
+        if (firstItem == false && nextComma == string::npos) {
+            list = list + "<li>" + unformattedList.substr(comma + 2) + "</li>" + "\n";
+        }
+        else if (firstItem == false) {
+            list = list + "<li>" + unformattedList.substr(comma + 2, (nextComma - comma - 2)) + "</li>" + "\n";
+        }
+        firstItem = false;
+    }
+
+    if(unformattedList.substr(0, 3) == "*UL") {
+        list = "<ul>\n" + list + "</ul>\n";
+    }
+    else {
+        list = "<ol>\n" + list + "</ol>\n";
+    }
+
+    return list;
+}
