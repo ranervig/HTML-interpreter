46267fb61a81fe027b3ab27e49cc22d087108bed
Syed Rehman <60289718+cliche37@users.noreply.github.com>
2021-04-10 07:39:52 -0500

    Add files via upload
    -Moved list generator to new header file.
    -Removed list generator function from main/driver

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,9 @@
 #include <fstream>
 #include <deque>
 #include <string>
+
+#include "html_interpreter.h"
+
 using namespace std;
 
 deque<string> readFile();
@@ -9,77 +12,42 @@ string stringToList(string unformattedList);
 
 int main() {
 
-	deque<string> inputDeque = readFile();
-	while (!inputDeque.empty()) {
-		cout << inputDeque.front() << endl;
-		inputDeque.pop_front();
-	}
+    deque<string> inputDeque = readFile();
+    
+    deque<string> convertedDeque = htmlInterpreter(inputDeque);
+    
+    while (!convertedDeque.empty()) {
+        cout << convertedDeque.front() << endl;
+        convertedDeque.pop_front();
+    }
 
-	return 0;
+    return 0;
 }
 
 
 //Robby Nervig
 deque<string> readFile() {
-	fstream file;
-	string fileName;
-	deque<string> fileQueue;
+    fstream file;
+    string fileName;
+    deque<string> fileQueue;
 
-	cout << "Please enter the name of the file to convert, including extension(.txt):";
-	cin >> fileName;
-	file.open(fileName, ios::in);
+    cout << "Please enter the name of the file to convert, including extension(.txt):";
+    cin >> fileName;
+    file.open(fileName, ios::in);
 
-	//checks if file opened correctly 
-	while (!file.good()) {
-		cout << "Please enter a valid file name, including extension(.txt):";
-		cin >> fileName;
-		file.open(fileName, ios::in);
-	}
-
-	string line;
-	//pull lines from file and add to deque
-	while (getline(file, line)) {
-		fileQueue.push_back(line);
-	}
-
-	file.close();
-	return fileQueue;
-}
-
-
-
-string stringToList(string unformattedList) { // Noah Chung
-    // takes a string and returns a string in HTML list format that can be added to the queue
-    // tested it with items of different size and with lists with 3 and 4 items
-    const int AFTER_TAG = 4;
-    string list;
-    size_t nextComma = 0;
-    bool firstItem = true;
-
-    while (nextComma != string::npos) {
-        string dimension;
-        size_t comma = unformattedList.find(", ", nextComma);
-        nextComma = unformattedList.find(", ", nextComma + 2);
-
-        if (firstItem == true) {
-            list = list + "<li>" + unformattedList.substr(AFTER_TAG, nextComma - 4) + "</li>" + "\n";
-        }
-
-        if (firstItem == false && nextComma == string::npos) {
-            list = list + "<li>" + unformattedList.substr(comma + 2) + "</li>" + "\n";
-        }
-        else if (firstItem == false) {
-            list = list + "<li>" + unformattedList.substr(comma + 2, (nextComma - comma - 2)) + "</li>" + "\n";
-        }
-        firstItem = false;
+    //checks if file opened correctly
+    while (!file.good()) {
+        cout << "Please enter a valid file name, including extension(.txt):";
+        cin >> fileName;
+        file.open(fileName, ios::in);
     }
 
-    if(unformattedList.substr(0, 3) == "*UL") {
-        list = "<ul>\n" + list + "</ul>\n";
-    }
-    else {
-        list = "<ol>\n" + list + "</ol>\n";
+    string line;
+    //pull lines from file and add to deque
+    while (getline(file, line)) {
+        fileQueue.push_back(line);
     }
 
-    return list;
+    file.close();
+    return fileQueue;
 }
